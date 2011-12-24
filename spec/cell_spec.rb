7290bf52989267b466153f9f7637198dd7576072
Karthik Sirasanagandla <karthiks@thoughtworks.com>
2011-12-24 17:38:26 +0530

    Added spec for cell comparability

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -4,4 +4,24 @@ describe Cell do
     cell = Cell.new(1,2)
     cell.to_s.should == "(1,2)"
   end
+
+  context "comparable" do
+    it "should consider y-position first for comparison" do
+      big_y_cell = Cell.new(1,2)
+      small_y_cell = Cell.new(2,1)
+      (big_y_cell > small_y_cell).should be_true
+    end
+
+    it "should consider x-position when y-positions are equal" do
+      small_x_cell = Cell.new(1,1)
+      big_x_cell = Cell.new(2,1)
+      (big_x_cell > small_x_cell).should be_true
+    end
+
+    it "should return equal when both positions are same" do
+      cell_1 = Cell.new(1,1)
+      cell_2 = Cell.new(1,1)
+      (cell_1 == cell_2).should be_true
+    end
+  end
 end
