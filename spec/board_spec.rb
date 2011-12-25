442cb3765bcf70b8ad10cabff7ae1de3a7e8e621
Karthik Sirasanagandla <karthiks@thoughtworks.com>
2011-12-25 20:30:36 +0530

    Spec refactoring: Changing mock based to state based

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -16,11 +16,8 @@ describe Board do
     it "should mark cell state to DIES, 
     if neighbour count IS LESS THAN 2 [under-population]" do
       life_positions = [[0,0]]
-      mock_cell = mock('Mock-Cell')
-      Cell.should_receive(:new).with(0,0).and_return(mock_cell)
-      mock_cell.should_receive(:neighbours_count).and_return(1)
-      mock_cell.should_receive(:state=).with(Cell::DIES)
-      mock_cell.should_receive(:position).and_return(Position.new(0,0))
+      cell = Cell.new(0,0)
+      Cell.should_receive(:new).with(0,0).and_return(cell)
       board = Board.new(life_positions)
       board.mark
     end
